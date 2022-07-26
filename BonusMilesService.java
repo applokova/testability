1aac847a467d95ae45e1cba6604277d338c318c3
applokova <stasya133@mail.ru>
2022-07-26 18:23:00 +0300

    v1

---
+++ BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int coast) {
+        int result;
+        if (coast >= 20) {
+            result = coast / 20;
+        } else result = 0;
+        return result;
+    }
+}
